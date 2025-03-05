abb7ecdf9a4124461c7e190cd3ad960d2fedde24
slataa <slataa@garnetvalley.org>
2025-03-05 08:52:34 -0500

    updated instructions

--- BasicMath.java
+++ BasicMath.java
@@ -1,1 +1,24 @@
 
+public class BasicMath
+{
+	public static void main(String[] args)
+	{
+		//variable types:
+		/*
+		 * integer (int) - whole numbers
+		 * decimal (double) - decimal numbers
+		 * True/False (boolean)
+		 * character (char) - single character
+		 * words (String) - multiple characters and /or spaces
+		 */
+		
+		//How to declare a variable:
+		//VARIABLE_TYPE  VARIABLE_NAME;
+		//1) create a variable of each type listed above:
+		
+		
+		//How to initialize a variable:
+		//VARIABLE_NAME = VALUE;
+		//2) initialize the variables you declared above
+	}
+}
